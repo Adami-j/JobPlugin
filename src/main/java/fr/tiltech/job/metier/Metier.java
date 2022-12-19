f0f5a24e725f5d5834aa4f61e3fc10ee1bf569ff
juju <julien.adami@etu.iut-tlse3.fr>
2022-12-19 21:12:31 +0100

    ajout attributs et constructeur Metier.java

--- src/main/java/fr/tiltech/job/metier/Metier.java
+++ src/main/java/fr/tiltech/job/metier/Metier.java
@@ -1,4 +1,21 @@
 package fr.tiltech.job.metier;
 
+import org.bukkit.entity.Player;
+
 public abstract class Metier {
+
+    private String nomMetier;
+
+    private int niveau;
+
+    private Player player;
+
+    public Metier(String nomMetier, int niveau, Player player) {
+        this.nomMetier = nomMetier;
+        this.niveau = niveau;
+        this.player = player.getPlayer();
+    }
+
+
+
 }
