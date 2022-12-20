f2951a91c28d03f99d0af768698e5ff3ac862cb6
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2022-12-20 18:56:34 +0100

    Ajout du fichier config.yml + reloadCommand pas encore fonctionnelle

--- src/main/java/fr/tiltech/job/Listener/PlayerJoin.java
+++ src/main/java/fr/tiltech/job/Listener/PlayerJoin.java
@@ -1,5 +1,6 @@
-package fr.tiltech.job;
+package fr.tiltech.job.Listener;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,14 +17,14 @@ public class PlayerJoin implements Listener {
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         if (!player.isOp()) {
-            player.sendMessage(ChatColor.GREEN + "[3iLTechServ] Bienvenue " + player.getName());
-            if (!player.hasPermission("") {
-                player.sendMessage("§cTu n'as pas de métier ! Choisis-en un parmi la liste :");
+            player.sendMessage("§3</§63iLTechServ§3> §6Bienvenue §3" + player.getName());
+            if (!player.hasPermission("")) {
+                player.sendMessage("§8> §cTu n'as pas de métier ! Choisis-en un parmi la liste :");
                 player.sendMessage("§7- §aMineur");
                 player.sendMessage("§7- §aGuerrier");
                 player.sendMessage("§7- §aChimiste");
-                player.sendMessage("§7- §aFermier");
-                player.sendMessage("§cClique sur le métier que tu voudrais exercer.");
+                player.sendMessage("§7- §aFermier\n");
+                player.sendMessage("§8> §cClique sur le métier que tu voudrais exercer.");
             }
         }
     }
--- src/main/java/fr/tiltech/job/Main.java
+++ src/main/java/fr/tiltech/job/Main.java
@@ -2,22 +2,32 @@ package fr.tiltech.job;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import fr.tiltech.job.Listener.PlayerJoin;
 
 public final class Main extends JavaPlugin {
+    FileConfiguration config = getConfig();
 
     private static boolean papiEnabled = false;
 
     @Override
     public void onEnable() {
+        // Enable Configuration
+        config.options().copyDefaults(true);
+        saveConfig();
+
+
         System.out.println(ChatColor.GREEN + "Enable plugin");
         Bukkit.getPluginManager().registerEvents(new PlayerJoin(), this);
 
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             this.getLogger().info("PlaceholderAPI detected.");
             Main.papiEnabled = true;
+        } else {
+            this.getLogger().info("PlaceholderAPI not detected.");
         }
 
     }
